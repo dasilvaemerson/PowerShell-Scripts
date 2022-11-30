47952aeb4360e941af7d667ced4a322007280b91
Emerson da Silva <95425629+dasilvaemerson@users.noreply.github.com>
2022-11-30 13:36:20 +0100

    Implement "Delete files older than X days"

--- Compress Files/Compress-Files-Maintain-Date-Delete-Original-File.ps1
+++ Compress Files/Compress-Files-Maintain-Date-Delete-Original-File.ps1
@@ -7,20 +7,23 @@ SCRIPT FUNCTIONALITY:
 
 NOTE:
     ! ✓ Ignore compressed files (.bz2, .gz, .zip)
+    ! ✓ Ignore folders
     ! ✓ Delete old file
     ! ✓ Keep folder structure
     ! ✓ Do not delete folders
+    ! ✓ Replace ZIP if file already exists
+    ! ✓ Delete files older than X days
 
-    # Error detection if ZIP already exists?
+    # Code Refactoring: Make a Function out of the script with dinamic Path and Date.
 #>
 
 
 # List of folders to be excluded of the script, so the entire folder is not compressed on D:\
-$excluded = @("*.bz2", "*.gz", "*.zip", "apache", "mysql", "tomcat", "web", "dump")
-
+$excluded = @("*.bz2", "*.gz", "*.zip")
+$limit = (Get-Date).AddDays(-30)
 $SourceFolder = "D:\ServerLog"
 
-Get-ChildItem $SourceFolder -Exclude $excluded -Recurse | select * |
+Get-ChildItem $SourceFolder -file -Exclude $excluded -Recurse | select * |
   Foreach-Object {
     # Store the data of the original file.
     $CreationTime = $_.CreationTime
@@ -35,7 +38,7 @@ Get-ChildItem $SourceFolder -Exclude $excluded -Recurse | select * |
     $zipPath = $DirectoryName + "\" + $zip
 
     # File compression
-    Compress-Archive -Path $_.FullName -DestinationPath $zipPath
+    Compress-Archive -Path $_.FullName -DestinationPath $zipPath -Update
 
     # Change the properties of the Zip-File as the same of the original file
     Set-ItemProperty -Path $zipPath -Name CreationTime -Value $CreationTime
@@ -43,6 +46,10 @@ Get-ChildItem $SourceFolder -Exclude $excluded -Recurse | select * |
     Set-ItemProperty -Path $zipPath -Name LastAccessTime -Value $LastAcessTime
 
     # Remove old file after creating a compressed version
-    $_ | Remove-Item -WhatIf
+    $_ | Remove-Item
 
   }
+
+# Remove every file where the modification date is older than the $limit (For example, older than 30 days)
+Get-ChildItem -Path $SourceFolder -Recurse -Force | Where-Object { !$_.PSIsContainer -and $_.LastWriteTime -lt $limit } | Remove-Item -Force
+
